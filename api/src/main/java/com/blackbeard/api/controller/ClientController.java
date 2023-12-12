e45c58394e1b03e22becafadb7425940f9d35f81
Carlos Henrique <97392902+kralluz@users.noreply.github.com>
2023-12-12 20:14:42 -0300

    feat: implementação da rota de get e getById
    ps. para a rota de getById tive que usar os querys params pois os path params estavam apresentando instabilidade nos retornos em json

--- api/src/main/java/com/blackbeard/api/controller/ClientController.java
+++ api/src/main/java/com/blackbeard/api/controller/ClientController.java
@@ -2,6 +2,7 @@ package com.blackbeard.api.controller;
 
 import com.blackbeard.api.exception.ApiException;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,6 +39,24 @@ public class ClientController {
         client.setTel(clientDTO.getTel());
 
         Client savedClient = clientRepository.save(client);
-        return ResponseEntity.ok(savedClient);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedClient);
+    }
+
+    @GetMapping
+    public ResponseEntity<Client[]> findAllClients(){
+        Client[] clients = clientRepository.findAll();
+        return ResponseEntity.ok(clients);
+    }
+
+    @GetMapping("/find")
+    public ResponseEntity<Client> findClientById(@RequestParam int id){
+        Client client = clientRepository.findById(id);
+        return ResponseEntity.ok(client);
+    }
+
+    @PostMapping
+    public ResponseEntity<Client> updateClientById(@RequestParam int id){
+        Client client = clientRepository.update(id);
+        return ResponseEntity.ok(client);
     }
 }
--- api/src/main/java/com/blackbeard/api/repository/ClientRepository.java
+++ api/src/main/java/com/blackbeard/api/repository/ClientRepository.java
@@ -9,6 +9,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,13 +28,12 @@ public class ClientRepository {
     }
 
     public Client save(Client client) {
-        String sql = "INSERT INTO clients (name, tel) VALUES (?, ?) RETURNING *";
-        logger.info("Salvando cliente: {}", client);
+        String queryString = "INSERT INTO clients (name, tel) VALUES (?, ?) RETURNING *";
 
         try {
             assert dataSource != null;
             try (Connection conn = dataSource.getConnection();
-                 PreparedStatement ps = conn.prepareStatement(sql)) {
+                 PreparedStatement ps = conn.prepareStatement(queryString)) {
                 ps.setString(1, client.getName());
                 ps.setString(2, client.getTel());
 
@@ -42,11 +45,6 @@ public class ClientRepository {
                             client.setName(rs.getString("name"));
                             client.setTel(rs.getString("tel"));
                             client.setCreatedAt(String.valueOf(rs.getTimestamp("createdAt")));
-                            client.setUpdatedAt(String.valueOf(rs.getTimestamp("updatedAt")));
-
-                            logger.debug("Cliente inserido: ID={}, Nome={}, Telefone={}, Criado em={}",
-                                    client.getId(), client.getName(), client.getTel(),
-                                    client.getCreatedAt());
                         }
                     }
                 } else {
@@ -54,10 +52,116 @@ public class ClientRepository {
                 }
             }
         } catch (SQLException e) {
+
             logger.error("Erro ao salvar cliente:", e);
         }
 
         return client;
     }
 
+    public Client[] findAll() {
+        String queryString = "SELECT * FROM clients";
+        List<Client> clientList = new ArrayList<>();
+        try {
+            assert dataSource != null;
+
+            try (Connection conn = dataSource.getConnection();
+                 PreparedStatement ps = conn.prepareStatement(queryString)) {
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    Client client = new Client();
+                    client.setId(rs.getInt("id"));
+                    client.setName(rs.getString("name"));
+                    client.setTel(rs.getString("tel"));
+                    client.setCreatedAt(String.valueOf(rs.getTimestamp("createdAt")));
+                    client.setUpdatedAt(String.valueOf(rs.getTimestamp("updatedAt")));
+                    clientList.add(client);
+                }
+            }
+        } catch (SQLException e) {
+            logger.error("Erro ao buscar clientes:", e);
+        }
+
+        return clientList.toArray(new Client[0]);
+    }
+
+    public Client findById(int id) {
+        try {
+            String queryString = "SELECT * FROM clients WHERE id = ?";
+
+            assert dataSource != null;
+
+            Client client = null;
+            try (Connection conn = dataSource.getConnection();
+                 PreparedStatement ps = conn.prepareStatement(queryString)) {
+                ps.setInt(1, id);
+                boolean hasResultSet = ps.execute();
+                if (hasResultSet) {
+                    try (ResultSet rs = ps.getResultSet()) {
+                        if (rs.next()) {
+                            client = new Client();
+                            client.setId(rs.getInt("id"));
+                            client.setName(rs.getString("name"));
+                            client.setTel(rs.getString("tel"));
+                            client.setCreatedAt(String.valueOf(rs.getTimestamp("createdAt")));
+                            if (Objects.equals(String.valueOf(rs.getTimestamp("createdAt")), String.valueOf(rs.getTimestamp("updatedAt")))){
+                                client.setUpdatedAt(null);
+                            }else{
+                                client.setUpdatedAt(String.valueOf(rs.getTimestamp("updatedAt")));
+                            }
+                        }
+                    }
+                }
+            } catch (SQLException e) {
+                logger.error("Erro ao buscar cliente:", e);
+            }
+            return client;
+
+        } catch (NumberFormatException e) {
+            logger.error("ID fornecido não é um número inteiro válido", e);
+            // Você pode retornar null ou lançar uma exceção personalizada aqui
+            return null;
+        }
+    }
+
+    public Client update(int id){
+        try {
+            String queryString = "UPDATE clients\n" +
+                    "SET column1 = value1, column2 = value2, ...\n" +
+                    "WHERE id = ?;";
+
+            assert dataSource != null;
+
+            Client client = null;
+            try (Connection conn = dataSource.getConnection();
+                 PreparedStatement ps = conn.prepareStatement(queryString)) {
+                ps.setInt(1, id);
+                boolean hasResultSet = ps.execute();
+                if (hasResultSet) {
+                    try (ResultSet rs = ps.getResultSet()) {
+                        if (rs.next()) {
+                            client = new Client();
+                            client.setId(rs.getInt("id"));
+                            client.setName(rs.getString("name"));
+                            client.setTel(rs.getString("tel"));
+                            client.setCreatedAt(String.valueOf(rs.getTimestamp("createdAt")));
+                            if (Objects.equals(String.valueOf(rs.getTimestamp("createdAt")), String.valueOf(rs.getTimestamp("updatedAt")))){
+                                client.setUpdatedAt(null);
+                            }else{
+                                client.setUpdatedAt(String.valueOf(rs.getTimestamp("updatedAt")));
+                            }
+                        }
+                    }
+                }
+            } catch (SQLException e) {
+                logger.error("Erro ao buscar cliente:", e);
+            }
+            return client;
+
+        } catch (NumberFormatException e) {
+            logger.error("ID fornecido não é um número inteiro válido", e);
+            // Você pode retornar null ou lançar uma exceção personalizada aqui
+            return null;
+        }
+    }
 }
